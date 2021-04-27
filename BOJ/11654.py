9cbf2f72e118a75ebf949d5c69925c28b01357bf
hodoodang <xogh4096@gmail.com>
2021-04-27 21:40:42 +0900

    add BOJ 11654

---
+++ BOJ/11654.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""아스키 코드
+
+알파벳 소문자, 대문자, 숫자 0-9중 하나가 주어졌을 때,
+주어진 글자의 아스키 코드값을 출력하는 프로그램을 작성하시오.
+
+Example:
+    def solution():
+        result = do_something()
+        return result
+
+    if __name__ == '__main__':
+
+        solution()
+
+"""
+
+
+solution=ord
+
+
+if __name__ == '__main__':
+    print(solution((input())))
