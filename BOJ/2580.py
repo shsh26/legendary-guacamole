4969bcb296b5336927ab40cba226ccc1d600347b
hodoodang <xogh4096@gmail.com>
2021-05-25 23:56:02 +0900

    modified BOJ 2580

--- BOJ/2580.py
+++ BOJ/2580.py
@@ -27,78 +27,50 @@ Example:
         solution()
 
 """
-import sys
-
-# r = sys.stdin.readline
 
 
-# 가로 체크
-def horizontal(x, val):
-    if val in sudoku[x]:
-        return False
-    return True
+class Sudoku:
+    def __init__(self):
+        self._board = [list(map(int, input().split())) for _ in range(9)]
+        self._zeros = [(i, j) for i in range(9) for j in range(9) if self._board[i][j] == 0]
+        self._flag = False
 
+    def is_promising(self, i, j):
+        promising = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
-# 세로 체크
-def vertical(y, val):
-    for i in range(9):
-        if val == sudoku[i][y]:
-            return False
-    return True
+        for k in range(9):
+            if self._board[i][k] in promising:
+                promising.remove(self._board[i][k])
+            if self._board[k][j] in promising:
+                promising.remove(self._board[k][j])
 
+        i //= 3
+        j //= 3
 
-# 3x3 체크
-def bythree(x, y, val):
-    nx = x // 3 * 3
-    ny = y // 3 * 3
-    for i in range(3):
-        for j in range(3):
-            if val == sudoku[nx + i][ny + j]:
-                return False
-    return True
+        for p in range(i * 3, (i + 1) * 3):
+            for q in range(j * 3, (j + 1) * 3):
+                if self._board[p][q] in promising:
+                    promising.remove(self._board[p][q])
 
+        return promising
 
-def DFS(index):
-    if index == len(zeros):
-        for row in sudoku:
-            for n in row:
-                print(n, end=" ")
-            print()
-        sys.exit(0)
-    else:
-        for i in range(1, 10):
-            nx = zeros[index][0]
-            ny = zeros[index][1]
+    def dfs(self, x):
+        if self._flag:
+            return
 
-            # 세로, 가로, 3x3에 내가 대체하려고하는 숫자가 존재하는지 확인
-            if horizontal(nx, i) and vertical(ny, i) and bythree(nx, ny, i):
-                sudoku[nx][ny] = i
-                DFS(index + 1)
-                sudoku[nx][ny] = 0
+        if x == len(self._zeros):
+            for row in self._board:
+                print(*row)
+            self._flag = True
+            return
+        else:
+            (i, j) = self._zeros[x]
+            promising = self.is_promising(i, j)
 
-
-# sudoku = [list(map(int, r().split())) for _ in range(9)]
-# zeros = [(i, j) for i in range(9) for j in range(9) if sudoku[i][j] == 0]
-# DFS(0)
-
-
-def isPossible(cols: list, level: int):
-    for i in range(level):
-        if cols[i] == cols[level] or abs(level - i) == abs(cols[level] - cols[i]):
-            return False
-    return True
-
-
-def solution(cols: list, level: int):
-    if level == 4:
-        for i in range(4):
-            print(cols[i], end=' ')
-        print()
-    else:
-        for i in range(4):
-            cols[i] = i
-            if isPossible(cols, level):
-                solution(cols, level + 1)
+            for num in promising:
+                self._board[i][j] = num
+                self.dfs(x + 1)
+                self._board[i][j] = 0
 
 
 def short_code():
@@ -109,5 +81,5 @@ def short_code():
 
 
 if __name__ == '__main__':
-    board = [0] * 4
-    solution(cols=board, level=0)
+    sudoku = Sudoku()
+    sudoku.dfs(0)
