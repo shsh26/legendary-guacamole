90dfa1ef8ffce5d097e40746bb0f9e7038756686
hodoodang <xogh4096@gmail.com>
2021-06-11 23:59:45 +0900

    add BOJ 1149

---
+++ BOJ/1149.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+"""RGB거리
+
+RGB거리에는 집이 N개 있다. 거리는 선분으로 나타낼 수 있고, 1번 집부터 N번 집이 순서대로 있다.
+
+집은 빨강, 초록, 파랑 중 하나의 색으로 칠해야 한다.
+각각의 집을 빨강, 초록, 파랑으로 칠하는 비용이 주어졌을 때, 아래 규칙을 만족하면서 모든 집을 칠하는 비용의 최솟값을 구해보자.
+
+- 1번 집의 색은 2번 집의 색과 같지 않아야 한다.
+- N번 집의 색은 N-1번 집의 색과 같지 않아야 한다.
+- i(2 ≤ i ≤ N-1)번 집의 색은 i-1번, i+1번 집의 색과 같지 않아야 한다.
+
+첫째 줄에 집의 수 N(2 ≤ N ≤ 1,000)이 주어진다.
+둘째 줄부터 N개의 줄에는 각 집을 빨강, 초록, 파랑으로 칠하는 비용이 1번 집부터 한 줄에 하나씩 주어진다.
+집을 칠하는 비용은 1,000보다 작거나 같은 자연수이다.
+
+첫째 줄에 모든 집을 칠하는 비용의 최솟값을 출력한다.
+
+Example:
+    def solution():
+        result = do_something()
+        return result
+
+    if __name__ == '__main__':
+
+        solution()
+
+"""
+import sys
+
+
+def solution(n: int):
+    rgb = []
+    for _ in range(n):
+        rgb.append(list(map(int, sys.stdin.readline().split())))
+
+    for i in range(1, n):
+        rgb[i][0] = min(rgb[i - 1][1], rgb[i - 1][2]) + rgb[i][0]
+        rgb[i][1] = min(rgb[i - 1][0], rgb[i - 1][2]) + rgb[i][1]
+        rgb[i][1] = min(rgb[i - 1][0], rgb[i - 1][1]) + rgb[i][2]
+    return min(rgb[n - 1][0], rgb[n - 1][1], rgb[n - 1][2])
+
+
+def short_code():
+    """숏 코드
+    """
+
+
+if __name__ == '__main__':
+    print(solution(int(input())))
