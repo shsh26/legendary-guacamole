6103a0148898913d25290ba73b24d4d40269ba95
hodoodang <xogh4096@gmail.com>
2021-06-12 00:05:56 +0900

    add BOJ 1149 short_code

--- BOJ/1149.py
+++ BOJ/1149.py
@@ -33,7 +33,6 @@ def solution(n: int):
     rgb = []
     for _ in range(n):
         rgb.append(list(map(int, sys.stdin.readline().split())))
-
     for i in range(1, n):
         rgb[i][0] = min(rgb[i - 1][1], rgb[i - 1][2]) + rgb[i][0]
         rgb[i][1] = min(rgb[i - 1][0], rgb[i - 1][2]) + rgb[i][1]
@@ -44,6 +43,9 @@ def solution(n: int):
 def short_code():
     """숏 코드
     """
+    I = sys.stdin.readline;c = [0, 0]
+    for _ in range(int(I())): r, g, b = map(int, I().split());c = min(c[1:]) + r, min(c[0::2]) + g, min(c[:2]) + b
+    print(min(c))
 
 
 if __name__ == '__main__':
