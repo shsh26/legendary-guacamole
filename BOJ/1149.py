bf588b15d62b71e13bb7ebcadba8ec0cee76e3aa
hodoodang <xogh4096@gmail.com>
2021-06-12 00:00:53 +0900

    modified BOJ 1149

--- BOJ/1149.py
+++ BOJ/1149.py
@@ -37,7 +37,7 @@ def solution(n: int):
     for i in range(1, n):
         rgb[i][0] = min(rgb[i - 1][1], rgb[i - 1][2]) + rgb[i][0]
         rgb[i][1] = min(rgb[i - 1][0], rgb[i - 1][2]) + rgb[i][1]
-        rgb[i][1] = min(rgb[i - 1][0], rgb[i - 1][1]) + rgb[i][2]
+        rgb[i][2] = min(rgb[i - 1][0], rgb[i - 1][1]) + rgb[i][2]
     return min(rgb[n - 1][0], rgb[n - 1][1], rgb[n - 1][2])
 
 
