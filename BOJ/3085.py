5c1120feb669823ba0337c26730b48778ae56146
hodoodang <xogh4096@gmail.com>
2021-12-15 00:37:52 +0900

    modified BOJ 3085

--- BOJ/3085.py
+++ BOJ/3085.py
@@ -21,41 +21,42 @@
 N = int(input())
 board = [list(input()) for _ in range(N)]
 ans = 1
-# print(board)
 
 
-def search():
+def search(y, x):
     global ans
 
-    for j in range(N):
-        cnt = 1
-        for i in range(1, N):
-            if board[i - 1][j] == board[i][j]:
-                cnt += 1
-                ans = max(ans, cnt)
-            else:
-                cnt = 1
+    for i in range(y, y + 2):
+        if i < N:
+            cnt = 1
+            for j in range(1, N):
+                if board[i][j - 1] == board[i][j]:
+                    cnt += 1
+                    ans = max(ans, cnt)
+                else:
+                    cnt = 1
 
-    for i in range(N):
-        cnt = 1
-        for j in range(1, N):
-            if board[i][j - 1] == board[i][j]:
-                cnt += 1
-                ans = max(ans, cnt)
-            else:
-                cnt = 1
+    for j in range(x, x + 2):
+        if j < N:
+            cnt = 1
+            for i in range(1, N):
+                if board[i - 1][j] == board[i][j]:
+                    cnt += 1
+                    ans = max(ans, cnt)
+                else:
+                    cnt = 1
 
 
 for i in range(N):
     for j in range(N):
-        if j != N - 1:
+        if j < N - 1:
             board[i][j], board[i][j + 1] = board[i][j + 1], board[i][j]
-            search()
+            search(i, j)
             board[i][j], board[i][j + 1] = board[i][j + 1], board[i][j]
 
-        if i != N - 1:
+        if i < N - 1:
             board[i][j], board[i + 1][j] = board[i + 1][j], board[i][j]
-            search()
+            search(i, j)
             board[i][j], board[i + 1][j] = board[i + 1][j], board[i][j]
 
 print(ans)
